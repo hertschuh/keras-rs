579ef504ad35b909e115634a1eb149e1649fc364
Fabien Hertschuh <1091026+hertschuh@users.noreply.github.com>
2024-10-21 16:28:36 -0700

    Initial commit with one class and one example.
    - Renamed to keras-rs in README
    - Added `BruteForceRetrieval` layer
    - Added basic retrieval example
    - Added standard shell script to format, lint, generate the api with namex
    - Added config for black, isort, flake8 and mypy
    - Added standard `.gitignore`
    
    Unit tests and example were run with all 3 backends (tf, jax and torch)

---
+++ keras_rs/src/types.py
@@ -0,0 +1,25 @@
+"""Type definitions."""
+
+from typing import Any, Optional, Sequence
+
+"""
+A tensor in any of the backends.
+
+We do not define it explicitly to not require all the backends to be installed
+and imported. The explicit definition would be:
+```
+Union[
+  tensorflow.Tensor,
+  tensorflow.RaggedTensor,
+  tensorflow.SparseTensor,
+  tensorflow.IndexedSlices,
+  jax.Array,
+  jax.experimental.sparse.JAXSparse,
+  torch.Tensor,
+  keras.KerasTensor,
+]
+```
+"""
+Tensor = Any
+
+TensorShape = Sequence[Optional[int]]
