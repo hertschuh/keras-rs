a0801439f3a9b602613ec6186c9840ef9a6298a3
Abheesht Sharma <abheesht@google.com>
2025-08-20 13:48:24 +0530

    Fix dense features

--- examples/ml_perf/dataloader.py
+++ examples/ml_perf/dataloader.py
@@ -123,7 +123,7 @@ class DataLoader:
         example = tf.io.parse_single_example(example, feature_spec)
 
         # Dense features
-        dense_input = tf.stack(
+        dense_input = tf.concatenate(
             [
                 tf.reshape(example[dense_feature], [self.file_batch_size, 1])
                 for dense_feature in self.dense_features
