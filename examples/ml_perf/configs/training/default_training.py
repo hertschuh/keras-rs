b07891dc7916eaec89c725e038494eb507dcfa5f
Fabien Hertschuh <1091026+hertschuh@users.noreply.github.com>
2025-09-23 12:38:07 -0700

    batch size 32.

--- examples/ml_perf/configs/training/default_training.py
+++ examples/ml_perf/configs/training/default_training.py
@@ -3,5 +3,5 @@ from keras.utils import Config
 # === Training Hyperparameters ===
 training_config = Config()
 training_config.learning_rate = 0.005
-training_config.global_batch_size = 64
+training_config.global_batch_size = 32
 training_config.num_epochs = 1
