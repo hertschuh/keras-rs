f42d6a590ae18d9844f70b31014e036afd711b92
Fabien Hertschuh <1091026+hertschuh@users.noreply.github.com>
2025-09-23 13:02:28 -0700

    TF version MLPerf.

--- examples/ml_perf/main.py
+++ examples/ml_perf/main.py
@@ -1,8 +1,20 @@
 import argparse
 import importlib
 import os
+import libtpu
 
-os.environ["KERAS_BACKEND"] = "jax"
+os.environ["PJRT_DEVICE"] = "TPU"
+os.environ["NEXT_PLUGGABLE_DEVICE_USE_C_API"] = "true"
+os.environ["TF_PLUGGABLE_DEVICE_LIBRARY_PATH"] = libtpu.get_library_path()
+os.environ["TF_XLA_FLAGS"] = (
+    "--tf_mlir_enable_mlir_bridge=true "
+    "--tf_mlir_enable_convert_control_to_data_outputs_pass=true "
+    "--tf_mlir_enable_merge_control_flow_pass=true"
+)
+
+import tensorflow as tf
+
+os.environ["KERAS_BACKEND"] = "tensorflow"
 
 import keras
 
@@ -36,11 +48,43 @@ def main(
     file_batch_size,
     num_epochs,
 ):
-    # Set DDP as Keras distribution strategy
-    devices = keras.distribution.list_devices(device_type="tpu")
-    distribution = keras.distribution.DataParallel(devices=devices)
-    keras.distribution.set_distribution(distribution)
-    num_processes = distribution._num_process
+    print("### TF", tf.__version__)
+
+    topology = tf.tpu.experimental.initialize_tpu_system(resolver)
+
+    device_assignment = tf.tpu.experimental.DeviceAssignment.build(
+        topology, num_replicas=tpu_metadata.num_cores
+    )
+    strategy = tf.distribute.TPUStrategy(
+        resolver, experimental_device_assignment=device_assignment
+    )
+
+    print("### Resolver")
+    resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu="")
+    # print("### Connect")
+    # tf.config.experimental_connect_to_cluster(resolver)
+    # This is the TPU initialization code that has to be at the beginning.
+    print("### Initialize")
+    tf.tpu.experimental.initialize_tpu_system(resolver)
+    print("### Devices", tf.config.list_logical_devices('TPU'))
+
+    print("### TPU metadata")
+    tpu_metadata = resolver.get_tpu_system_metadata()
+    device_assignment = tf.tpu.experimental.DeviceAssignment.build(
+        topology, num_replicas=tpu_metadata.num_cores
+    )
+    print("### Strategy...")
+    strategy = tf.distribute.TPUStrategy(
+        resolver, experimental_device_assignment=device_assignment
+    )
+
+    print("### strategy", type(strategy), strategy)
+    print("### strategy.extended", strategy.extended)
+    print("### strategy.extended.tpu_hardware_feature", strategy.extended.tpu_hardware_feature)
+    print("### strategy.extended.tpu_hardware_feature.embedding_feature", strategy.extended.tpu_hardware_feature.embedding_feature)
+    print("### strategy.extended.tpu_hardware_feature.num_embedding_devices_per_chip", strategy.extended.tpu_hardware_feature.num_embedding_devices_per_chip)
+
+    num_processes = 1  # TODO distribution._num_process
 
     per_host_batch_size = global_batch_size // num_processes
 
@@ -93,23 +137,24 @@ def main(
     # embedding feature inputs using the distributed embedding layer defined
     # inside the model class.
     print("===== Initialising model =====")
-    model = DLRMDCNV2(
-        large_emb_feature_configs=feature_configs,
-        small_emb_features=small_emb_features,
-        embedding_dim=embedding_dim,
-        bottom_mlp_dims=bottom_mlp_dims,
-        top_mlp_dims=top_mlp_dims,
-        num_dcn_layers=num_dcn_layers,
-        dcn_projection_dim=dcn_projection_dim,
-        seed=SEED,
-        dtype="float32",
-        name="dlrm_dcn_v2",
-    )
-    model.compile(
-        loss=keras.losses.BinaryCrossentropy(),
-        optimizer=keras.optimizers.Adagrad(learning_rate=learning_rate),
-        metrics=[keras.metrics.BinaryAccuracy()],
-    )
+    with strategy.scope():
+        model = DLRMDCNV2(
+            large_emb_feature_configs=feature_configs,
+            small_emb_features=small_emb_features,
+            embedding_dim=embedding_dim,
+            bottom_mlp_dims=bottom_mlp_dims,
+            top_mlp_dims=top_mlp_dims,
+            num_dcn_layers=num_dcn_layers,
+            dcn_projection_dim=dcn_projection_dim,
+            seed=SEED,
+            dtype="float32",
+            name="dlrm_dcn_v2",
+        )
+        model.compile(
+            loss=keras.losses.BinaryCrossentropy(),
+            optimizer=keras.optimizers.Adagrad(learning_rate=learning_rate),
+            metrics=[keras.metrics.BinaryAccuracy()],
+        )
 
     # === Load dataset ===
     print("===== Loading dataset =====")
@@ -123,47 +168,48 @@ def main(
         label=label,
         training=True,
     ).create_dataset(
-        process_id=distribution._process_id,
+        # process_id=distribution._process_id, # TODO
         num_processes=num_processes,
         shuffle_buffer=shuffle_buffer,
     )
     # For the multi-host case, the dataset has to be distributed manually.
     # See note here:
     # https://github.com/keras-team/keras-rs/blob/main/keras_rs/src/layers/embedding/base_distributed_embedding.py#L352-L363.
-    if num_processes > 1:
-        train_ds = distribution.distribute_dataset(train_ds)
-        # eval_ds = distribution.distribute_dataset(eval_ds)
-        distribution.auto_shard_dataset = False
+    # if num_processes > 1:
+    #     train_ds = distribution.distribute_dataset(train_ds)
+    #     # eval_ds = distribution.distribute_dataset(eval_ds)
+    #     distribution.auto_shard_dataset = False
 
     # Print one sample.
     for element in train_ds.take(1):
         print(">>> train sample", element[0])
 
-    def generator(dataset, training=False):
-        """Converts tf.data Dataset to a Python generator and preprocesses
-        large embedding features.
-        """
-        for features, labels in dataset:
-            preprocessed_large_embeddings = model.embedding_layer.preprocess(
-                features["large_emb_inputs"], training=training
-            )
+    # def generator(dataset, training=False):
+    #     """Converts tf.data Dataset to a Python generator and preprocesses
+    #     large embedding features.
+    #     """
+    #     for features, labels in dataset:
+    #         preprocessed_large_embeddings = model.embedding_layer.preprocess(
+    #             features["large_emb_inputs"], training=training
+    #         )
 
-            x = {
-                "dense_input": features["dense_input"],
-                "large_emb_inputs": preprocessed_large_embeddings,
-                "small_emb_inputs": features["small_emb_inputs"],
-            }
-            y = labels
-            yield (x, y)
+    #         x = {
+    #             "dense_input": features["dense_input"],
+    #             "large_emb_inputs": preprocessed_large_embeddings,
+    #             "small_emb_inputs": features["small_emb_inputs"],
+    #         }
+    #         y = labels
+    #         yield (x, y)
 
-    train_generator = generator(train_ds, training=True)
+    # train_generator = generator(train_ds, training=True)
     # eval_generator = generator(eval_ds, training=False)
-    for first_batch in train_generator:
-        model(first_batch[0])
-        break
+    with strategy.scope():
+        for element in train_ds.take(1):
+            model(element[0])
 
     # Train the model.
-    model.fit(train_generator, epochs=num_epochs)
+    with strategy.scope():
+        model.fit(train_ds, epochs=num_epochs)
 
 
 if __name__ == "__main__":
