af9ba920b580bdd5a4524da06cd116df64e3c6ec
Abheesht Sharma <abheesht@google.com>
2025-10-18 23:37:05 +0530

    Refactor configs to one file

--- examples/ml_perf/configs/training/default_training.py
+++
@@ -1,9 +0,0 @@
-from keras.utils import Config
-
-# === Training Hyperparameters ===
-training_config = Config()
-training_config.learning_rate = 0.0034
-training_config.global_batch_size = 128
-# Set num_steps in the main config file instead of num_epochs, because we are
-# using a Python generator.
-# training_config.num_epochs = 1
