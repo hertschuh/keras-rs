b552276a0621ea8243c6fdf8e5c17119df3f6f7e
Abheesht Sharma <abheesht@google.com>
2025-10-22 01:04:22 +0530

    Reduce bsz

--- examples/ml_perf/configs/v6e_16.py
+++ examples/ml_perf/configs/v6e_16.py
@@ -192,7 +192,7 @@ model_config.dcn_projection_dim = 512
 # === Training ===
 training_config = Config()
 training_config.learning_rate = 0.0034
-training_config.global_batch_size = 1024
+training_config.global_batch_size = 4096
 # Set `num_steps` in the main config file instead of num_epochs, because we are
 # using a Python generator.
 training_config.num_steps = 20
