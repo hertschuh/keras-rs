e7451f0efe50ff6901576c9f8537a2dca9fb7cd3
Abheesht Sharma <abheesht@google.com>
2025-10-24 16:04:11 +0530

    Debug

--- examples/ml_perf/main.py
+++ examples/ml_perf/main.py
@@ -232,7 +232,7 @@ def main(
 
 if __name__ == "__main__":
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
         datefmt="%H:%M:%S",
     )
